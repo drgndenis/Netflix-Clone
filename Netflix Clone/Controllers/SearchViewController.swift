218e74a8e4c58e358f3439390482fdf7aa9b9e49
drgndenis <serkis-denis.dragan@ogr.iuc.edu.tr>
2023-07-26 14:14:26 +0300

    Coding SearchViewController

--- Netflix Clone/Controllers/SearchViewController.swift
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -8,9 +8,71 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    private var titles: [Title] = [Title]()
+    
+    private let discoverMoviesURL = "\(Constants.baseURL)/3/discover/movie?api_key=\(Constants.API_KEY)&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_watch_monetization_types=flatrate"
+
+    private let discoverTable: UITableView = {
+
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
         view.backgroundColor = .systemBackground
+        
+        view.addSubview(discoverTable)
+        discoverTable.dataSource = self
+        discoverTable.delegate = self
+        
+        fetchDiscoverMovies()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        discoverTable.frame = view.bounds
+    }
+    
+    private func fetchDiscoverMovies() {
+        APICaller.shared.getData(url: URL(string: discoverMoviesURL)) { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.discoverTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else { return UITableViewCell() }
+        
+        let title = titles[indexPath.row]
+        let model = TitleViewModel(titleName: title.originalTitle ?? title.originalLanguage ?? "Unknown", posterURL: title.posterPath ?? "")
+        cell.configure(with: model)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
     }
 }
